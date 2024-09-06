3155883a52b2da0740ba36ea2a2e151c291e1749
Marcus Klein <marcus.klein@open-xchange.com>
2024-09-06 14:00:07 +0200

    files for reproducing issue with Renovate

---
+++ Temurin.kt
@@ -0,0 +1,9 @@
+class Temurin {
+    private val link1 =
+        "https://github.com/adoptium/temurin8-binaries/releases/download/" +
+        "jdk8u412-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u412b08.tar.gz"
+
+    private val link2 =
+        "https://github.com/adoptium/temurin11-binaries/releases/download/" +
+        "jdk-11.0.24+8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.24_8.tar.gz"
+}
